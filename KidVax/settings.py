03b56773e4d940f9971a90950dfd4d7f16eaf4af
Gabriel <opaleye.dayo@yahoo.com>
2021-07-07 12:55:37 +0300

    Debug set to True

--- KidVax/settings.py
+++ KidVax/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool,  default=False)
+DEBUG = config('DEBUG', cast=bool,  default=True)
 
 ALLOWED_HOSTS = ['*']
 
